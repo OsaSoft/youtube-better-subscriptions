3764cf7666702cc584fb5edff1eaf3646db73139
Oscar Hernández <OsaSofto@gmail.com>
2018-01-29 11:23:09 +0100

    Added comments to main script
    Added explaining comments to bits of code in the main script,
    along with a few possible TODOs for the future.
    Removed commented out lines that shouldnt have been commited.

--- subs.js
+++ subs.js
@@ -1,15 +1,15 @@
-var delaySeconds = 3000;
+var delaySeconds = 3000; //TODO: configurable?
 var hidden = [];
-var newLayout = document.querySelectorAll(".feed-item-container .yt-shelf-grid-item").length == 0;
+var newLayout = document.querySelectorAll(".feed-item-container .yt-shelf-grid-item").length == 0; //is it the new (~fall 2017) YT layout?
 
 function isWatched(item) {
     return (!newLayout &&
         (item.getElementsByClassName("watched").length > 0 ||
-            item.getElementsByClassName("contains-percent-duration-watched").length > 0)) ||
+            item.getElementsByClassName("contains-percent-duration-watched").length > 0)) || //has "WATCHED" on thumbnail
         (newLayout &&
-            (item.querySelectorAll("yt-formatted-string.style-scope.ytd-thumbnail-overlay-playback-status-renderer").length > 0 ||
-                item.querySelectorAll("#progress.style-scope.ytd-thumbnail-overlay-resume-playback-renderer").length > 0) ||
-                item.hasAttribute("is-dismissed"))
+            (item.querySelectorAll("yt-formatted-string.style-scope.ytd-thumbnail-overlay-playback-status-renderer").length > 0 || //has "WATCHED" on thumbnail
+                item.querySelectorAll("#progress.style-scope.ytd-thumbnail-overlay-resume-playback-renderer").length > 0) || //has progress bar on thumbnail
+            item.hasAttribute("is-dismissed")) //also hide empty blocks left in by pressing "HIDE" button
 }
 
 function removeWatched() {
@@ -49,9 +49,7 @@ function addButton() {
         subGridButtonContainer.setAttribute("class", "yt-uix-menu-top-level-button yt-uix-menu-top-level-flow-button");
     }
 
-    // var span = document.createElement("span");
-    // span.appendChild();
-    subGridButtonContainer.appendChild(document.createTextNode("Hide watched"));
+    subGridButtonContainer.appendChild(document.createTextNode("Hide watched")); //TODO: translations
 
     var subGridCheckbox = document.createElement("input");
     subGridCheckbox.setAttribute("id", "subs-grid");
