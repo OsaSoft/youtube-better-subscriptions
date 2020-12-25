OsaSoft/youtube-better-subscriptions

commit 2dd78f10d548216c837510956ccb60e371c8355d

Adding feature: Hide watched videos on Youtube homepage (#84)
Resolves #80
Resolves #73

pageHandler.js
@@ -10,6 +10,7 @@ function initExtension() {
         "subscriptions": "/feed/subscriptions",
         "video": "/watch",
         "channel": "/videos",
+        "home": ""
     });
 
     function handlePageChange(page) {
@@ -29,6 +30,9 @@ function initExtension() {
                     break;
                 case PAGES.video:
                     onVideoPage();
+                    break
+                case PAGES.home:
+                    initSubs();
                     break;
                 default:
                     if (page.includes(PAGES.channel) && settings["settings.hide.watched.support.channel"])

subs-ui.js
@@ -265,7 +265,7 @@ function removeWatchedAndAddButton() {
         if (dismissableDiv.querySelector("#" + button) != null) {
             continue;
         } else {
-            dismissableDiv = dismissableDiv.firstChild;
+            let dismissableDiv = item.querySelector("#dismissable");
 
             if (!isPolymer) {
                 dismissableDiv = dismissableDiv.firstChild;

queries.js
@@ -1,6 +1,6 @@
 function vidQuery() {
     if (isPolymer) {
-        return "ytd-grid-video-renderer.style-scope.ytd-grid-renderer:not(." + HIDDEN_CLASS + ")";
+        return "ytd-grid-video-renderer.style-scope.ytd-grid-renderer:not(." + HIDDEN_CLASS + ")" + ", ytd-rich-item-renderer.style-scope.ytd-rich-grid-renderer:not(." + HIDDEN_CLASS + ")";
     } else {
         return ".feed-item-container .yt-shelf-grid-item:not(." + HIDDEN_CLASS + ")";
     }

commit 5458c0a2efc89fdb7bec7ea24b1d31d86020c493

Fix missing Mark as watched button caused by 2dd78f10d548216c837510956ccb60e371c8355d

common.js
@@ -52,3 +52,12 @@ function hide(element) {
 function show(element) {
     element.classList.remove("hidden");
 }
+
+function getCurrentPage() {
+    let path = window.location.pathname;
+    if (path != null) {
+        return path.replace(/\/$/, "");
+    }
+
+    return "";
+}

pageHandler.js
@@ -13,9 +13,9 @@ function initExtension() {
         "home": ""
     });
 
-    function handlePageChange(page) {
+    function handlePageChange() {
         //remove trailing /
-        page = page.replace(/\/$/, "");
+        let page = getCurrentPage();
 
         log("Page was changed to " + page);
 
@@ -57,7 +57,7 @@ function initExtension() {
                     mutations.forEach((mutationRecord) => {
                         //is page fully loaded?
                         if (mutationRecord.target.attributes["aria-valuenow"].value === "100") {
-                            handlePageChange(window.location.pathname);
+                            handlePageChange();
                         }
                     });
                 });
@@ -67,7 +67,7 @@ function initExtension() {
             }
 
             //first page doesnt trigger the event, so lets do it manually
-            handlePageChange(window.location.pathname);
+            handlePageChange();
         }
     }
 

subs-ui.js
@@ -254,7 +254,7 @@ function removeWatchedAndAddButton() {
     for (let item of els) {
         let stored = getVideoId(item) in storage;
         let dismissableDiv = item.firstElementChild;
-        let button = stored? MARK_UNWATCHED_BTN : MARK_WATCHED_BTN;
+        let button = stored ? MARK_UNWATCHED_BTN : MARK_WATCHED_BTN;
 
         if (stored && hideWatched) {
             hideItem(item);
@@ -265,7 +265,7 @@ function removeWatchedAndAddButton() {
         if (dismissableDiv.querySelector("#" + button) != null) {
             continue;
         } else {
-            let dismissableDiv = item.querySelector("#dismissable");
+            dismissableDiv = dismissableDiv.firstChild;
 
             if (!isPolymer) {
                 dismissableDiv = dismissableDiv.firstChild;