afecd5c6709266cf6a0dc1f803697f393ea90b5d
Oscar Hernández <OsaSofto@gmail.com>
2023-02-11 23:06:56 +0100

    Rewrite of video functionality pt.2 - Refactor video handling in subs page (#128)

--- subs-ui.js
+++ subs-ui.js
@@ -12,12 +12,6 @@ const COLLAPSE_CLASS = PREFIX + "collapse-section";
 
 let addedElems = [];
 
-function hideItem(item) {
-    hidden.push(item);
-    item.style.display = 'none';
-    item.classList.add(HIDDEN_CLASS);
-}
-
 function showWatched() {
     log("Showing watched videos");
 
@@ -234,13 +228,11 @@ function removeWatchedAndAddButton() {
 
         if (!vid.isStored && isYouTubeWatched(item)) {
             vid.markWatched();
-        } else if (vid.isStored && hideWatched) {
-            vid.hide();
-            hiddenCount++;
-        } else if (hidePremieres && isPremiere(item)) {
-            vid.hide();
-            hiddenCount++;
-        } else if (hideShorts && isShort(item)) {
+        } else if (
+                (hideWatched && vid.isStored) ||
+                (hidePremieres && vid.isPremiere) ||
+                (hideShorts && vid.isShort)
+        ) {
             vid.hide();
             hiddenCount++;
         }
--- videos/SubscriptionsVideo.js
+++ videos/SubscriptionsVideo.js
@@ -1,58 +1,11 @@
-function getVideoIdFromUrl(url) {
-    if (url.includes("shorts")) {
-        return url.split("shorts/")[1].split("&")[0];
-    } else {
-        return url.split("=")[1].split("&")[0];
-    }
-}
-
-function getVideoId(item) {
-    return getVideoIdFromUrl(item.querySelectorAll("a")[0].getAttribute("href"));
-}
-
-function changeMarkWatchedToMarkUnwatched(item) {
-    // find Mark as watched button and change it to Unmark as watched
-    let metaDataLine = item.querySelector("#" + METADATA_LINE);
-    if (metaDataLine != null) {
-        let dismissibleDiv = metaDataLine.parentNode;
-        dismissibleDiv.removeChild(metaDataLine);
-
-        let markUnwatchedBtn = buildMarkWatchedButton(dismissibleDiv, item, getVideoId(item), false);
-        dismissibleDiv.appendChild(markUnwatchedBtn);
-    }
-}
-
-class SubscriptionVideo {
+class SubscriptionVideo extends Video {
     constructor(containingDiv) {
-        this.containingDiv = containingDiv;
-        this.videoId = getVideoId(containingDiv);
-        this.isStored = this.videoId in storage;
+        super(containingDiv);
         this.contentDiv = this.containingDiv.firstElementChild;
-        this.button = this.isStored ? MARK_UNWATCHED_BTN : MARK_WATCHED_BTN;
     }
 
     hasButton() {
-        return this.contentDiv.querySelector("#" + this.button) != null
-    }
-
-    hide() {
-        hideItem(this.containingDiv);
-    }
-
-    markWatched() {
-        changeMarkWatchedToMarkUnwatched(this.containingDiv);
-
-        if (hideWatched) {
-            this.hide();
-            processSections();
-        }
-
-        setVideoInStorage(this.videoId);
-        this.isStored = true;
-    }
-
-    markUnwatched() {
-        getStorage().remove(this.videoId);
+        return this.contentDiv.querySelector("#" + this.buttonId) != null
     }
 
     addButton() {
