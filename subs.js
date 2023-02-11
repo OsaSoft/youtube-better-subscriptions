34aa1175be543c2807c8491a885932ecdb9598e1
Oscar Hernández <OsaSofto@gmail.com>
2023-02-11 23:20:40 +0100

    #111 Fix 'off' state for YT's watched status toggle setting not working (#129)

--- subs.js
+++ subs.js
@@ -7,7 +7,7 @@ let intervalId = null;
 
 function isYouTubeWatched(item) {
     let ytWatchedPercentThreshold = settings["settings.mark.watched.youtube.watched"];
-    return ytWatchedPercentThreshold !== null && (
+    return ytWatchedPercentThreshold === true && (
             (item.querySelectorAll("yt-formatted-string.style-scope.ytd-thumbnail-overlay-playback-status-renderer").length > 0 || //has "WATCHED" on thumbnail
                     item.querySelectorAll("#progress.style-scope.ytd-thumbnail-overlay-resume-playback-renderer").length > 0) || //has progress bar on thumbnail TODO allow percentage threshold
             item.hasAttribute("is-dismissed") //also hide empty blocks left in by pressing "HIDE" button
