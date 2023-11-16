7bedc93558e59a1ea42945d518b47dbc15e916fd
Valentin Mocanu <valentin@mocanew.dev>
2023-11-16 15:31:15 +0200

    Configured eslint based on .editorconfig

--- browser.ts
+++ browser.ts
@@ -1,10 +1,11 @@
+// eslint-disable-next-line import/no-mutable-exports
 let brwsr: typeof browser;
 try {
     brwsr = browser;
 }
 catch (error) {
     if (error instanceof ReferenceError) {
-        brwsr = chrome;
+        brwsr = chrome as any; // eslint-disable-line @typescript-eslint/no-explicit-any
     }
 }
 
