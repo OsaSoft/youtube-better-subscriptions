03127326da53f37c88b35f791aafe3d7beede272
example <example@example.com>
2024-09-29 22:50:10 -0400

    hide older disabled by default + update options page ui

--- common.js
+++ common.js
@@ -2,11 +2,9 @@ const PREFIX = "osasoft-better-subscriptions_";
 
 const DEFAULT_SETTINGS = {
     "settings.hide.watched.label": true,
-    "settings.hide.older.label": true,
     "settings.hide.watched.all.label": true,
     "settings.hide.watched.ui.stick.right": false,
     "settings.hide.watched.default": true,
-    "settings.hide.older.default": false,
     "settings.hide.watched.keep.state": true,
     "settings.hide.watched.refresh.rate": 3000,
     "settings.mark.watched.youtube.watched": false,
@@ -16,7 +14,8 @@ const DEFAULT_SETTINGS = {
     "settings.hide.watched.auto.store": true,
     "settings.hide.premieres": false,
     "settings.hide.shorts": false,
-    "settings.hide.older.cutoff":"2 Weeks"
+    "settings.hide.older": false,
+    "settings.hide.older.cutoff":"All"
 };
 
 const SETTINGS_KEY = "settings";
